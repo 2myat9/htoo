6db69d072e0bac01274aa484517ec1e102332b81
Htoomyat Naing <2myat9@gmail.com>
2023-06-23 17:15:29 -0700

    remove border from card

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -2,7 +2,7 @@ const Card = ({ link, title, body }) => {
   return (
     <div className="flex flex-col items-center">
       <div className="rounded-lg p-1 bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-teal-600 via-violet-800 to-purple-600">
-        <div className="block max-w-sm p-6 bg-teal-50 text-black border rounded-lg hover:bg-transparent hover:text-teal-50">
+        <div className="block max-w-sm p-6 bg-teal-50 text-black rounded-lg transition duration-200 ease-in-out hover:bg-transparent hover:text-teal-50">
           <h5 className="mb-2 text-xl font-bold tracking-tight">{title}</h5>
           <p className="font-normal">{body}</p>
         </div>
